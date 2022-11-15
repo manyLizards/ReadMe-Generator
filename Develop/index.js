edb6596dc9d4847aed9b47909726deac23e8a1a9
Jordan <jnbarringer96@gmail.com>
2022-11-15 11:53:12 -0600

    added function to initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -64,7 +64,13 @@ function writeToFile(fileName, data) {
 };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
