fa4d593feb6e15be090de0c9c4f5ca0d01cc1bcd
Jordan <jnbarringer96@gmail.com>
2022-11-15 11:43:09 -0600

    added questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require('fs'); //file system
 
 // TODO: Create an array of questions for user input
 inquirer
     .createPromptModule([
-        {
+        { //project name
             type: 'input',
             name: 'project-title',
             message: 'What is the name of the project?',
+        },
+        { //description
+            type: 'input',
+            name: 'description',
+            message: 'Please describe the project',
+        },
+        { //installation instructions
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install the project?',
+        },
+        { //usage
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use the project?',
+        },
+        { //contribution guidelines
+            type: 'input',
+            name: 'contribution-guidelines',
+            message: 'How do you contribute to this project?',
+        },
+        { //test instructions
+            type: 'input',
+            name: 'test-instructions',
+            message: 'How do you test this project?',
+        },
+        { //license - not finished - need to make it multiple choice
+            type: 'input',
+            name: 'license',
+            message: 'Which license',
+        },
+        { //github username
+            type: 'input',
+            name: 'Github-username',
+            message: 'What is your github username?',
+        },
+        { //email address
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
         },
     ])
 
